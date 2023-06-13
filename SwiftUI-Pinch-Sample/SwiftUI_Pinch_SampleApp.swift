b21db48eef0b743f5f60685e0361c729687e3a1e
ekayaint <ekayaint@gmail.com>
2023-06-13 13:52:52 +0300

    Initial Commit

---
+++ SwiftUI-Pinch-Sample/SwiftUI_Pinch_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Pinch_SampleApp.swift
+//  SwiftUI-Pinch-Sample
+//
+//  Created by ekayaint on 13.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Pinch_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
